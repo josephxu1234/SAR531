09e5f1aec30d0e646f83cb82f60fd6b4d9ff2680
josephxu1234 <josephxu1234@gmail.com>
2025-10-30 14:48:41 -0400

    Initialized git for SAR531

---
+++ tests/test_import_minigrid.py
@@ -0,0 +1,14 @@
+import gymnasium as gym
+import minigrid
+
+def test_import_minigrid():
+    env = gym.make('MiniGrid-Empty-5x5-v0', render_mode='human')
+    obs, info = env.reset(seed = 100)
+
+    for _ in range(10):
+        action = env.action_space.sample()
+        obs, reward, terminated, truncated, info = env.step(action)
+        if terminated or truncated:
+            obs, info = env.reset()
+    env.close()
+    print('Successfully imported and ran MiniGrid environment.')
